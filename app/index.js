0c1816277d701975f9685c39fc14662bab21a104
James Almond <jamestalmond@gmail.com>
2018-01-03 12:58:18 +0000

    Destructuring arrays and objects

--- app/index.js
+++ app/index.js
@@ -1,2 +1,4 @@
-console.log('Hello from webpack with an automatic update!');
-console.log('Hello');
+let son = { name: 'Simba', parents: 2 };
+let name, parents;
+({ name, parents } = son);
+console.log(name, parents);
